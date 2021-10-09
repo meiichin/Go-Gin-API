37b810ec05a754b82f77c28d47bd1d42d6a9c4d1
meiichin <meiichan3017@gmail.com>
2021-10-09 18:35:45 +0700

    [Added] entity campaign

--- campaign/entity.go
+++ campaign/entity.go
@@ -1,1 +1,25 @@
 package campaign
+
+import "time"
+
+type Campaign struct {
+	ID               int
+	UserID           int
+	Name             string
+	ShortDescription string
+	Description      string
+	Perks            string
+	BackerCount      int
+	GoalAmount       int
+	CurrentAmout     int
+	Slug             string
+	CreatedAt        time.Time
+	UpdatedAt        time.Time
+}
+
+type CampaignImages struct {
+	ID         int
+	CampaignID int
+	FileName   string
+	IsPrimary  int
+}
