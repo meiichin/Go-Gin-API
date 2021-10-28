6a0f3116698f4c22ce754ed948e44ca8f8da33bf
meiichin <meiichan3017@gmail.com>
2021-10-28 13:28:30 +0700

    [Added] handler

--- handler/campaign.go
+++ handler/campaign.go
@@ -1,1 +1,32 @@
 package handler
+
+import (
+	"goginapi/campaign"
+	"goginapi/helper"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+type campainHandler struct {
+	service campaign.Service
+}
+
+func NewCampaignHanler(service campaign.Service) *campainHandler {
+	return &campainHandler{service}
+}
+
+func (h *campainHandler) GetCampaigns(c *gin.Context) {
+	userID, _ := strconv.Atoi(c.Query("user_id"))
+
+	campaigns, err := h.service.GetCampaigns(userID)
+	if err != nil {
+		response := helper.APIResponse("Error to get campaign", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := helper.APIResponse("List of campaigns", http.StatusOK, "success", campaigns)
+	c.JSON(http.StatusOK, response)
+}
